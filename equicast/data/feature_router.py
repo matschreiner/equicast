8f2e8c235dbd2e95b96d39b56dfbf156846b938b
Mathias Schreiner <matschreiner@gmail.com>
2025-12-11 14:05:57 +0100

    type hints

--- equicast/data/feature_router.py
+++ equicast/data/feature_router.py
@@ -1,9 +1,12 @@
 import torch
-from anemoi.utils.config import DotDict
+
+from equicast.data.feature_config import FeatureConfig
 
 
 class FeatureRouter:
-    def __init__(self, feature_config, name_to_index):
+    """Routes features to appropriate indices for model input/output."""
+
+    def __init__(self, feature_config: FeatureConfig, name_to_index: dict[str, int]):
         self.feature_config = feature_config
         self.name_to_index = name_to_index
 
@@ -11,10 +14,10 @@ class FeatureRouter:
         prognostic_idxs = self._get_data_idxs(feature_config.prognostic)
         diagnostic_idxs = self._get_data_idxs(feature_config.diagnostic)
 
-        self.in_idxs = forcing_idxs + prognostic_idxs
-        self.out_idxs = prognostic_idxs + diagnostic_idxs
+        self.in_idxs: list[int] = forcing_idxs + prognostic_idxs
+        self.out_idxs: list[int] = prognostic_idxs + diagnostic_idxs
 
-    def _get_data_idxs(self, names):
+    def _get_data_idxs(self, names: list[str]) -> list[int]:
         return [self.name_to_index[name] for name in names]
 
     def __call__(self, data):
