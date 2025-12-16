ea6545d4b769c3a38fad34b27b20b4d7a162c7c8
Mathias Schreiner <matschreiner@gmail.com>
2025-12-16 13:05:54 +0100

    expose modules in data

--- equicast/data/__init__.py
+++ equicast/data/__init__.py
@@ -1,1 +1,7 @@
-from equicast.data.feature_config import *
+"""Data handling components for equicast."""
+
+from equicast.data.data_handler import BaseDataHandler, DataHandler
+from equicast.data.feature_config import FeatureConfig
+from equicast.data.feature_router import FeatureRouter
+from equicast.data.processor import DataProcessor, SimpleDataProcessor
+from equicast.data.scaler import BaseScaler, Scaler
