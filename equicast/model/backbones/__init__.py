878ae06fcfa33f75257b6572e47c20f093770d46
Mathias Schreiner <matschreiner@gmail.com>
2025-12-16 15:27:02 +0100

    remove unused

--- equicast/model/backbones/__init__.py
+++ equicast/model/backbones/__init__.py
@@ -2,7 +2,8 @@
 
 from equicast.model.backbones.gnn import GNN
 from equicast.model.backbones.graphcast import Graphcast
-from equicast.model.backbones.painn import PaiNN
-from equicast.model.backbones.simple import SimpleBaseline
 
-__all__ = ["GNN", "Graphcast", "PaiNN", "SimpleBaseline"]
+#  from equicast.model.backbones.painn import PaiNN
+#  from equicast.model.backbones.simple import SimpleBaseline
+
+#  __all__ = ["GNN", "Graphcast", "PaiNN", "SimpleBaseline"]
