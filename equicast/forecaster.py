21eef02de378c9fc20db2177714144206c5839c9
Mathias Schreiner <matschreiner@gmail.com>
2025-12-18 16:58:03 +0100

    simplify forecaster

--- equicast/forecaster.py
+++ equicast/forecaster.py
@@ -20,7 +20,7 @@ class Forecaster:
         self.model = model
         self.logger = logger
 
-    def forecast(self, timeseries, graph, steps=-1, output_dir="."):
+    def forecast(self, timeseries, steps=-1, output_dir="."):
         """
         Autoregressively forecast for a given number of steps.
 
@@ -33,50 +33,93 @@ class Forecaster:
         Returns:
             List of predictions (model handles scaling internally)
         """
-        if steps == -1:
-            steps = len(timeseries) - 1
 
-        steps = 2
-        device = next(self.model.parameters()).device
-        timeseries = timeseries.to(device)
-        graph = graph.to(device)
         predictions = []
+        current_state = timeseries[0]
+        for step in range(10):
+            pred = self.model(current_state)
+            predictions.append(pred)
 
-        self.model.eval()
-        current_state = timeseries[0].unsqueeze(0)
+            next_state = timeseries[step + 1]
+            current_state = self.model.data_handler.update_state_with_prediction(
+                next_state,
+                pred,
+            )
 
-        with torch.no_grad():
-            for step in tqdm(range(steps), desc="Forecasting"):
-                _graph = graph.clone()
-                _graph["grid"].data = current_state
+        return predictions
 
-                prediction = self.model(_graph)
-                predictions.append(prediction)
-                current_state = (
-                    self.model.data_handler.update_state_with_prediction(
-                        timeseries[step + 1].unsqueeze(0),
-                        prediction,
-                    )
-                )
 
-        field = 1
-        preds = torch.stack(predictions)
-        preds_path = os.path.join(output_dir, "predictions.pt")
-        torch.save(preds, preds_path)
-        timeseries = self.model.data_handler.normalizer.transform(timeseries)
-        targets = self.model.data_handler.get_output_features(timeseries)
-        fields = preds[:, :, field]
-        mp4_path = os.path.join(output_dir, "forecast.mp4")
-        make_comparison_video(
-            fields.cpu().numpy(),
-            targets[:, :, field].cpu().numpy(),
-            graph["grid"].x.cpu().numpy(),
-            title="Forecasted field",
-            output_path=str(mp4_path),
-            fps=1,
-        )
-        if self.logger:
-            self.logger.log_artifact(mp4_path)
-            self.logger.log_artifact(preds_path)
+
+
+
+            __import__("pdb").set_trace()  # TODO delme
+            pred = self.model(current_state)
+
+            __import__("pdb").set_trace()  # TODO delme
+
+        __import__("pdb").set_trace()  # TODO delme
+        #  graph["grid"].data = timeseries
+        #  graph = self.model.data_handler.prepare_input(graph)
+        #  self.model.eval()
+        #
+        #  with torch.no_grad():
+        #      for step in tqdm(range(steps), desc="Forecasting"):
+        #          __import__("pdb").set_trace()
+        #          pred = model.predict(graph)
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #  if steps == -1:
+        #      steps = len(timeseries) - 1
+        #
+        #  steps = 2
+        #  device = next(self.model.parameters()).device
+        #  timeseries = timeseries.to(device)
+        #  graph = graph.to(device)
+        #
+        #  predictions = []
+        #
+        #  self.model.eval()
+        #  #  current_state = timeseries[0].unsqueeze(0)
+        #
+        #  self.model.data_handler.get_input_features(timeseries)
+        #  with torch.no_grad():
+        #      for step in tqdm(range(steps), desc="Forecasting"):
+        #          _graph = graph.clone()
+        #          _graph["grid"].data = current_state
+        #
+        #          prediction = self.model(_graph)
+        #          predictions.append(prediction)
+        #          current_state = (
+        #              self.model.data_handler.update_state_with_prediction(
+        #                  timeseries[step + 1].unsqueeze(0),
+        #                  prediction,
+        #              )
+        #          )
+        #
+        #  field = 1
+        #  preds = torch.stack(predictions)
+        #  preds_path = os.path.join(output_dir, "predictions.pt")
+        #  torch.save(preds, preds_path)
+        #  timeseries = self.model.data_handler.normalizer.transform(timeseries)
+        #  targets = self.model.data_handler.get_output_features(timeseries)
+        #  fields = preds[:, :, field]
+        #  mp4_path = os.path.join(output_dir, "forecast.mp4")
+        #  make_comparison_video(
+        #      fields.cpu().numpy(),
+        #      targets[:, :, field].cpu().numpy(),
+        #      graph["grid"].x.cpu().numpy(),
+        #      title="Forecasted field",
+        #      output_path=str(mp4_path),
+        #      fps=1,
+        #  )
+        #  if self.logger:
+        #      self.logger.log_artifact(mp4_path)
+        #      self.logger.log_artifact(preds_path)
 
         return preds
