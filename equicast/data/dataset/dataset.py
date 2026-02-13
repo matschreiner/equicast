62d3a0799a97a15a481a99752f55b023455dc6bf
Mathias Schreiner <matschreiner@gmail.com>
2026-02-13 23:29:57 +0100

    update dataset

--- equicast/data/dataset/dataset.py
+++ equicast/data/dataset/dataset.py
@@ -16,23 +16,22 @@ class AnemoiDataset(Dataset):
         self,
         path: str,
         graph_provider: BaseGraphProvider,
-        num_input_steps: int = 1,
     ):
         super().__init__()
         self.data = open_dataset(path)
         self.graph_provider = graph_provider
-        self.num_input_steps = num_input_steps
 
     def __len__(self):
         return len(self.data) - self.num_input_steps
 
     def __getitem__(self, idx):
-        data = torch.tensor(self.data[idx : idx + self.num_input_steps + 1]).squeeze().permute(0, 2, 1)
+        input_data = self.data[idx].squeeze()
+        target_data = self.data[idx + 1].squeeze()
 
         input_graph = self.graph_provider.get_graph(idx)
-        input_graph["grid"].data = data[:-1].squeeze()
+        input_graph["grid"].data = torch.from_numpy(input_data)
 
         target_graph = self.graph_provider.get_graph(idx)
-        target_graph["grid"].data = data[-1:].squeeze()
+        target_graph["grid"].data = torch.from_numpy(target_data)
 
         return {"input": input_graph, "target": target_graph}
