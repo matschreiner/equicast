093eb32a8a7003b16373055912eca6f7da84f091
Mathias Schreiner <matschreiner@gmail.com>
2026-02-13 18:20:12 +0100

    one processor in painn

--- config/backbones/painn.py
+++ config/backbones/painn.py
@@ -18,12 +18,10 @@ def backbone_config(dataset_path):
         edges=[
             ("grid", "to", "mesh"),
             ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
             ("mesh", "to", "grid"),
         ],
         input_nodes="grid",
-        hidden_dim=128,
+        hidden_dim=64,
     )
 
     return backbone, equivariant_loss_fn
