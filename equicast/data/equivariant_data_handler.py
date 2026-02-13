0fcf067344b5aac2f5ab1414a4cbdd9ebede6843
Mathias Schreiner <matschreiner@gmail.com>
2026-02-13 21:30:49 +0100

    pack once if possible

--- equicast/data/equivariant_data_handler.py
+++ equicast/data/equivariant_data_handler.py
@@ -64,8 +64,7 @@ class EquivariantGraphDataHandler(BaseDataHandler):
         diagnostic_vector_idxs = self._get_vector_idxs(feature_config.diagnostic_vector)
         self.in_vector_idxs = forcing_vector_idxs + prognostic_vector_idxs
         self.out_vector_idxs = prognostic_vector_idxs + diagnostic_vector_idxs
-        if self.in_vector_idxs == self.out_vector_idxs:
-            self.get_output_vectors = self.get_input_vectors
+        self._vectors_in_eq_out = self.in_vector_idxs == self.out_vector_idxs
 
         # Create normalizers
         data = open_dataset(dataset_path)
@@ -84,15 +83,14 @@ class EquivariantGraphDataHandler(BaseDataHandler):
         data[self.nodes]["residual_scalar"] = self.normalizer.normalize_output(output_scalars)
 
         # Pack and normalize vectors
-        input_vectors = self.get_input_vectors(raw)
-        norm_input_vectors = self.vector_normalizer.normalize_vectors(input_vectors)
+        norm_input_vectors = self.vector_normalizer.normalize_vectors(self.get_input_vectors(raw))
+        norm_output_vectors = (
+            self.vector_normalizer.normalize_vectors(self.get_output_vectors(raw))
+            if not self._vectors_in_eq_out
+            else norm_input_vectors
+        )
         data[self.nodes]["input_vector"] = norm_input_vectors
-
-        if self._shared_vector_idxs:
-            data[self.nodes]["residual_vector"] = norm_input_vectors
-        else:
-            output_vectors = self.get_output_vectors(raw)
-            data[self.nodes]["residual_vector"] = self.vector_normalizer.normalize_vectors(output_vectors)
+        data[self.nodes]["residual_vector"] = norm_output_vectors
 
         return data
 
