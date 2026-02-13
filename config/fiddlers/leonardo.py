1466ce8940474152c1d6a50e08913dcba5951414
Mathias Schreiner <matschreiner@gmail.com>
2026-02-13 21:10:07 +0100

    persistent workers off

--- config/fiddlers/leonardo.py
+++ config/fiddlers/leonardo.py
@@ -10,9 +10,7 @@ def fiddler(cfg: fdl.Config) -> None:
 
     cfg.model.backbone.data_handler.dataset_path = leonardo_era5
     cfg.dataloader.dataset.path = leonardo_era5
-    #  cfg.dataloader.dataset.subsample = 6
     cfg.dataloader.num_workers = 8
-    cfg.dataloader.persistent_workers = True
     cfg.dataloader.batch_size = 1
     cfg.trainer.strategy = "ddp"
 
