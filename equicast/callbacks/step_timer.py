97dc461a24fcf9e6c74863e1c4be46639c065210
Mathias Schreiner <matschreiner@gmail.com>
2026-02-15 00:56:10 +0100

    timer log all at once

--- equicast/callbacks/step_timer.py
+++ equicast/callbacks/step_timer.py
@@ -18,20 +18,17 @@ class StepTimer(Callback):
         if self._last_step_end is not None and trainer.global_step >= self.start_step:
             wait_time = self._step_start - self._last_step_end
             total_time = wait_time + self._last_opt_time
-            pl_module.log("performance/wait_time", wait_time, prog_bar=False, logger=True, on_step=True, on_epoch=False)
-            pl_module.log(
-                "performance/opt_step_time",
-                self._last_opt_time,
+            pl_module.log_dict(
+                {
+                    "performance/wait_time": wait_time,
+                    "performance/opt_step_time": self._last_opt_time,
+                    "performance/total_step_time": total_time,
+                    "performance/it_per_s": 1.0 / total_time,
+                },
                 prog_bar=False,
                 logger=True,
                 on_step=True,
                 on_epoch=False,
-            )
-            pl_module.log(
-                "performance/total_step_time", total_time, prog_bar=False, logger=True, on_step=True, on_epoch=False
-            )
-            pl_module.log(
-                "performance/it_per_s", 1.0 / total_time, prog_bar=False, logger=True, on_step=True, on_epoch=False
             )
 
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
