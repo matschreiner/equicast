0baa60f3e939a0475341f932111e3aa7c7ec652f
Mathias Schreiner <matschreiner@gmail.com>
2026-02-15 00:19:17 +0100

    add ema

--- equicast/callbacks/ema.py
+++ equicast/callbacks/ema.py
@@ -1,12 +1,11 @@
-from torch.optim.swa_utils import get_ema_multi_avg_fn
-
 from lightning.pytorch.callbacks import WeightAveraging
+from torch.optim.swa_utils import get_ema_multi_avg_fn
 
 
 class EMA(WeightAveraging):
     """Exponential Moving Average callback using Lightning's WeightAveraging."""
 
-    def __init__(self, decay: float = 0.999, update_after: int = 10000, **kwargs):
+    def __init__(self, decay: float = 0.999, update_after: int = 1000, **kwargs):
         super().__init__(multi_avg_fn=get_ema_multi_avg_fn(decay), **kwargs)
         self.update_after = update_after
 
