597add782abb85d33a67436661231750605d9569
Mathias Schreiner <matschreiner@gmail.com>
2026-02-14 18:35:25 +0100

    10000 steps in the scheduler

--- config/fiddlers/scheduler.py
+++ config/fiddlers/scheduler.py
@@ -8,6 +8,6 @@ from equicast.model.schedulers import WarmupCosineAnnealingLR
 def fiddler(cfg: fdl.Config) -> None:
     cfg.model.scheduler_factory = partial(
         WarmupCosineAnnealingLR,
-        warmup_steps=1000,
+        warmup_steps=10000,
         total_steps=864000,
     )
