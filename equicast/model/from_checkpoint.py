05c355fb6df83450a4fb7b29d48ed1c6c127917a
Mathias Schreiner <matschreiner@gmail.com>
2026-02-15 01:45:46 +0100

    weights only in pytorch 2.6

--- equicast/model/from_checkpoint.py
+++ equicast/model/from_checkpoint.py
@@ -5,4 +5,4 @@ from hydra.utils import get_class
 
 def from_checkpoint(model_cls: str, checkpoint_provider, *args, **kwargs: Any):
     ckpt = checkpoint_provider.get_checkpoint()
-    return model_cls.load_from_checkpoint(ckpt)
+    return model_cls.load_from_checkpoint(ckpt, weights_only=False)
