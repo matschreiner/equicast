f60f96dcd7c7521251b59df44d40194c19fc9745
Mathias Schreiner <matschreiner@gmail.com>
2026-02-16 15:33:07 +0100

    rename forecast

--- config/forecast.py
+++ config/forecast.py
@@ -17,7 +17,14 @@ def load_model(model_cls, checkpoint_provider):
 
 def main():
     dataset_path = "/home/masc/storage/era5-o96-2024-tail200-6h.zarr"
+    #  Scheduler
     checkpoint_path = "/leonardo/home/userexternal/jschrei1/equicast/mlruns/394873961037717851/80a28951aa9941578184d78d870e663e/checkpoints/latest.ckpt"
+    # EMA
+    #  checkpoint_path = "/leonardo/home/userexternal/jschrei1/equicast/mlruns/394873961037717851/bbfe37a6679f490abf1487be55124d16/checkpoints/latest.ckpt"
+    # VANILLA
+    #  checkpoint_path = "/leonardo/home/userexternal/jschrei1/equicast/mlruns/394873961037717851/a7004148bd5a4a2fae6062725d8dc090/checkpoints/latest.ckpt"
+
+    model_id = checkpoint_path.split("/")[-3]  # MLflow run ID
     host = "leonardo"
 
     dataset = AnemoiDataset(path=dataset_path, graph_provider=None, subsample=1)
@@ -52,6 +59,7 @@ def main():
         forecaster=forecaster,
         timeseries=timeseries,
         logger=logger,
+        model_id=model_id,
     )
 
     experiments.run_experiment(cfg)
