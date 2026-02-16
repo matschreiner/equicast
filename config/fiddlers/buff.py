f04944190bea69db63bfa92bcf4db5dd21b5151d
Mathias Schreiner <matschreiner@gmail.com>
2026-02-16 23:18:15 +0100

    buff painn

---
+++ config/fiddlers/buff.py
@@ -0,0 +1,28 @@
+import fiddle as fdl
+
+from equicast.model.backbones.painn import PaiNN
+
+
+def fiddler(cfg: fdl.Config) -> None:
+    cfg.model.backbone = fdl.Config(
+        PaiNN,
+        data_handler=cfg.model.backbone.data_handler,
+        edges=[
+            ("grid", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "mesh"),
+            ("mesh", "to", "grid"),
+        ],
+        input_nodes="grid",
+        hidden_dim=512,
+    )
