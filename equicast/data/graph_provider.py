538a08e71653f7a6b7116254aeaa12655b9ead6d
Mathias Schreiner <matschreiner@gmail.com>
2026-02-16 16:49:53 +0100

    better names and types

--- equicast/data/graph_provider.py
+++ equicast/data/graph_provider.py
@@ -1,18 +1,18 @@
 from abc import ABC, abstractmethod
 
 import torch
-from torch_geometric.data import Data, HeteroData
+from torch_geometric.data import HeteroData
 
 
 class BaseGraphProvider(ABC, torch.nn.Module):
     @abstractmethod
-    def get_graph(self, idx=None) -> Data: ...
+    def get_graph(self, idx=None) -> HeteroData: ...
 
 
 class StaticGraphProvider(BaseGraphProvider):
-    def __init__(self, path):
+    def __init__(self, graph_path):
         super().__init__()
-        self.graph = torch.load(path, weights_only=False)
+        self.graph = torch.load(graph_path, weights_only=False)
 
     def to(self, *args, **kwargs):
         super().to(*args, **kwargs)
