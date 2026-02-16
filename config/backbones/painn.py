286891a279c67dd9198bdcf81c7da8f46e86a9a0
Mathias Schreiner <matschreiner@gmail.com>
2026-02-16 17:45:29 +0100

    remove backbones

--- config/backbones/painn.py
+++ config/backbones/painn.py
@@ -1,18 +1,13 @@
 import fiddle as fdl
 import torch
 
-from equicast.data import EquivariantGraphDataHandler, FeatureConfig
 from equicast.model.backbones.painn import PaiNN
-from equicast.model.model import equivariant_loss_fn
 
 torch.set_float32_matmul_precision("medium")
 
 
-def backbone_config(dataset_path):
-    feature_config = fdl.Config(FeatureConfig.from_yaml, path="hydraconfig/features/base_equivariant.yaml")
-    data_handler = fdl.Config(EquivariantGraphDataHandler, feature_config=feature_config, dataset_path=dataset_path)
-
-    backbone = fdl.Config(
+def backbone_config(data_handler):
+    return fdl.Config(
         PaiNN,
         data_handler=data_handler,
         edges=[
@@ -24,7 +19,4 @@ def backbone_config(dataset_path):
         ],
         input_nodes="grid",
         hidden_dim=128,
-        #  hidden_dim=64,
     )
-
-    return backbone, equivariant_loss_fn
