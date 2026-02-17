33c8a5e354c0def23a32be94581f14c0890e8ff7
Mathias Schreiner <matschreiner@gmail.com>
2026-02-17 14:42:29 +0100

    add experiment name fiddler

--- config/fiddlers/experiment.py
+++ config/fiddlers/experiment.py
@@ -3,11 +3,11 @@ import fiddle as fdl
 from equicast.logger import MLFlowLogger
 
 
-def fiddler(cfg: fdl.Config) -> None:
+def fiddler(cfg: fdl.Config, name: str) -> None:
     logger = fdl.Config(
         MLFlowLogger,
         tracking_uri="file:./mlruns",
-        experiment_name="trash",
+        experiment_name=name,
     )
     cfg.logger = logger
     cfg.trainer.logger = logger
