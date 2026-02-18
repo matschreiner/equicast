316e91a8a46ed6814ec4afb1632307a526696f20
Mathias Schreiner <matschreiner@gmail.com>
2026-02-18 16:34:08 +0100

    use mlflow/mlflow.db

--- equicast/__init__.py
+++ equicast/__init__.py
@@ -1,9 +1,13 @@
+import mlflow
 import lovely_tensors as lt
 import torch
 
 lt.monkey_patch()
 
 DTYPE = torch.float32
-CHECKPOINT_PATH = "checkpoint"
+CHECKPOINT_PATH = "checkpoints"
+TRACKING_URI = "sqlite:///mlflow/mlflow.db"
 
-__all__ = ["DTYPE", "CHECKPOINT_PATH"]
+mlflow.set_tracking_uri(TRACKING_URI)
+
+__all__ = ["DTYPE", "CHECKPOINT_PATH", "TRACKING_URI"]
