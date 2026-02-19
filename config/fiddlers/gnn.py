531281a37ac19334377703d5004f9412b9788ffc
Mathias Schreiner <matschreiner@gmail.com>
2026-02-19 21:52:13 +0100

    add gnn fiddler

---
+++ config/fiddlers/gnn.py
@@ -0,0 +1,32 @@
+import fiddle as fdl
+
+from equicast import data
+from equicast.data import FeatureConfig, FeatureIndex
+from equicast.model.backbones.gnn import GNN
+from equicast.model.model import MSELoss
+
+FEATURE_CONFIG_PATH = "config/features/base.yaml"
+
+
+def fiddler(cfg: fdl.Config) -> None:
+    dataset_path = cfg.model.data_handler.dataset_path
+    feature_config = fdl.Config(FeatureConfig.from_yaml, path=FEATURE_CONFIG_PATH)
+    feature_indices = fdl.Config(
+        FeatureIndices.from_dataset,
+        dataset_path=dataset_path,
+        feature_config=feature_config,
+    )
+
+    cfg.model.backbone = fdl.Config(
+        GNN,
+        feature_indices=feature_indices,
+        edges=cfg.model.backbone.edges,
+        input_nodes="grid",
+        hidden_dim=int(cfg.model.backbone.hidden_dim * 1.471),
+    )
+    cfg.model.data_handler = fdl.Config(
+        data.GraphDataHandler,
+        feature_config=feature_config,
+        dataset_path=dataset_path,
+    )
+    cfg.model.loss_fn = fdl.Config(MSELoss)
