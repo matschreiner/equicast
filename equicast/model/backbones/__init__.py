71b6ba47c676bcb97a3230ee8b3d6e115bd11a21
Mathias Schreiner <matschreiner@gmail.com>
2026-02-19 21:51:36 +0100

    import painn

--- equicast/model/backbones/__init__.py
+++ equicast/model/backbones/__init__.py
@@ -1,11 +1,5 @@
 """Model backbones for equicast."""
 
-from equicast.model.backbones.equivariant_gnn import EquivariantGNN
 from equicast.model.backbones.gnn import GNN
-from equicast.model.backbones.simple_equivariant_gnn import SimpleEquivariantGNN
 from equicast.model.backbones.graphcast import Graphcast
-
-#  from equicast.model.backbones.painn import PaiNN
-#  from equicast.model.backbones.simple import SimpleBaseline
-
-#  __all__ = ["GNN", "Graphcast", "PaiNN", "SimpleBaseline"]
+from equicast.model.backbones.painn import PaiNN
