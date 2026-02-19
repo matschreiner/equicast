ec13ba270962c9258fb30287f19801fef3f84d1d
Mathias Schreiner <matschreiner@gmail.com>
2026-02-19 21:52:01 +0100

    fiddlers should fiddle

--- config/fiddlers/deep.py
+++ config/fiddlers/deep.py
@@ -1,23 +1,14 @@
 import fiddle as fdl
 
-from equicast.logger import MLFlowLogger
-from equicast.model.backbones.painn import PaiNN
-
 
 def fiddler(cfg: fdl.Config) -> None:
-    cfg.model.backbone = fdl.Config(
-        PaiNN,
-        feature_indices=cfg.model.backbone.feature_indices,
-        edges=[
-            ("grid", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "grid"),
-        ],
-        input_nodes="grid",
-        hidden_dim=128,
-    )
+    cfg.model.backbone.edges = [
+        ("grid", "to", "mesh"),
+        ("mesh", "to", "mesh"),
+        ("mesh", "to", "mesh"),
+        ("mesh", "to", "mesh"),
+        ("mesh", "to", "mesh"),
+        ("mesh", "to", "mesh"),
+        ("mesh", "to", "mesh"),
+        ("mesh", "to", "grid"),
+    ]
--- config/fiddlers/wide.py
+++ config/fiddlers/wide.py
@@ -1,26 +1,5 @@
 import fiddle as fdl
 
-from equicast.model.backbones.painn import PaiNN
-from equicast.model.schedulers import WarmupCosineAnnealingLR
-
 
 def fiddler(cfg: fdl.Config) -> None:
-    cfg.model.backbone = fdl.Config(
-        PaiNN,
-        feature_indices=cfg.model.backbone.feature_indices,
-        edges=[
-            ("grid", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "mesh"),
-            ("mesh", "to", "grid"),
-        ],
-        input_nodes="grid",
-        hidden_dim=256,
-    )
-
-    cfg.model.scheduler_factory = fdl.Partial(
-        WarmupCosineAnnealingLR,
-        warmup_steps=5000,
-        total_steps=500000,
-    )
+    cfg.model.backbone.hidden_dim = 256
