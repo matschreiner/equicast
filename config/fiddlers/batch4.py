7e97aa2cd61d8d939c4be110ee6e74b7712d1e1b
Mathias Schreiner <matschreiner@gmail.com>
2026-02-21 00:09:31 +0100

    batch4 scheduler

--- config/fiddlers/batch4.py
+++ config/fiddlers/batch4.py
@@ -1,5 +1,13 @@
 import fiddle as fdl
 
+from equicast.model.schedulers import WarmupCosineAnnealingLR
+
 
 def fiddler(cfg: fdl.Config) -> None:
     cfg.dataloader.batch_size = 4
+
+    cfg.model.scheduler_factory = fdl.Partial(
+        WarmupCosineAnnealingLR,
+        warmup_steps=5000,
+        total_steps=220000,
+    )
