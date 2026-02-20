a7abb14f1d6ec37ceb4c22a2f0c3fb85057dd554
Mathias Schreiner <matschreiner@gmail.com>
2026-02-20 17:25:55 +0100

    every 100

--- config/fiddlers/ema.py
+++ config/fiddlers/ema.py
@@ -4,7 +4,7 @@ from equicast.callbacks import EMA
 
 
 def fiddler(cfg: fdl.Config, decay: float = 0.9) -> None:
-    every_n = 1
-    start = 1
+    every_n = 100
+    start = 5
     ema_callback = fdl.Config(EMA, decay=decay, update_starting_at_step=start, update_every_n_steps=every_n)
     cfg.trainer.callbacks.append(ema_callback)
