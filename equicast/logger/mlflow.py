9efba3e45d72efcfe6a47b9c8952713a62ad02af
Mathias Schreiner <matschreiner@gmail.com>
2026-02-21 00:13:24 +0100

    no fancy prinitng

--- equicast/logger/mlflow.py
+++ equicast/logger/mlflow.py
@@ -25,10 +25,6 @@ def fix_artifact_location(experiment_name: str):
             yaml.dump(meta, f)
 
 
-def _is_global_zero() -> bool:
-    return os.environ.get("RANK", "0") == "0"
-
-
 def resolve_run(run_name_or_id: str) -> str:
     """Resolve a run name or ID to a run ID."""
     import mlflow
@@ -51,20 +47,13 @@ def resolve_run(run_name_or_id: str) -> str:
 class MLFlowLogger(MLFlowLoggerParent):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if _is_global_zero():
-            fix_artifact_location(self._experiment_name)
+        fix_artifact_location(self._experiment_name)
 
     def log_hyperparams(self, params):
-        first_call = not self._initialized
         try:
             super().log_hyperparams(params)
         except MlflowException:
             pass  # Ignore duplicate params when resuming a run
-        if first_call and _is_global_zero():
-            import mlflow
-            run_name = mlflow.get_run(self.run_id).info.run_name
-            line = f"MLflow run: {run_name}  |  id: {self.run_id}  |  experiment: {self._experiment_name}"
-            print(f"\n{line}\n{'-' * len(line)}\n")
 
     def after_save_checkpoint(self, filepath: str):
         try:
