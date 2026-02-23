28204e3829f4a3775aae967595a1da9e40f17dec
Mathias Schreiner <matschreiner@gmail.com>
2026-02-23 17:08:16 +0100

    no trans

--- equicast/model/backbones/__init__.py
+++ equicast/model/backbones/__init__.py
@@ -3,4 +3,5 @@
 from equicast.model.backbones.gnn import GNN
 from equicast.model.backbones.graphcast import GraphCast
 from equicast.model.backbones.painn import PaiNN
-from equicast.model.backbones.transformer import GraphTransformer
+
+#  from equicast.model.backbones.transformer import GraphTransformer
