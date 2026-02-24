1e21331793c3837c75dfec1f92c12cc5f05aacb7
Mathias Schreiner <matschreiner@gmail.com>
2026-02-24 16:06:30 +0100

    benchmark zarr

--- config/fiddlers/benchmark.py
+++ config/fiddlers/benchmark.py
@@ -1,31 +1,21 @@
 import fiddle as fdl
-from torch_geometric.loader import DataLoader
 
 from equicast import data
 from equicast.callbacks import StepTimer
-from equicast.data.dataset import BenchmarkDataset
+from equicast.data.dataset import AnemoiDataset
 
-GRAPH_PATH = "graph/aifs-graphcast-unnormed.pt"
+GRAPH_PATH = "resources/graph.pt"
+DATASET_PATH = "resources/benchmark.zarr"
 
 
 def fiddler(cfg: fdl.Config) -> None:
     graph_provider = fdl.Config(data.StaticGraphProvider, graph_path=GRAPH_PATH)
-    train_dataset = fdl.Config(
-        BenchmarkDataset,
-        samples_path="resources/samples",
-        graph_provider=graph_provider,
-        length=10000,
-    )
 
-    #  val_dataset = fdl.Config(
-    #      BenchmarkDataset,
-    #      samples_path="resources/val_samples",
-    #      graph_provider=graph_provider,
-    #      length=1,
-    #  )
-    #  val_dataloader = fdl.Config(DataLoader, val_dataset, num_workers=0, batch_size=1, shuffle=False)
-    #  cfg.val_dataloader = val_dataloader
-    #  cfg.trainer.val_check_interval = 3
+    train_dataset = fdl.Config(
+        AnemoiDataset,
+        path=DATASET_PATH,
+        graph_provider=graph_provider,
+    )
 
     cfg.dataloader.dataset = train_dataset
     cfg.dataloader.num_workers = 0
