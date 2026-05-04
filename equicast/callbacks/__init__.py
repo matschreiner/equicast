e16e240ae082c318b206e45e842c8b1d46ca60f1
Mathias Schreiner <matschreiner@gmail.com>
2026-05-04 11:54:41 +0200

    don't export ema

--- equicast/callbacks/__init__.py
+++ equicast/callbacks/__init__.py
@@ -1,5 +1,4 @@
 from equicast.callbacks.checkpoint import TimeDeltaCheckpoint
-from equicast.callbacks.ema import EMA
 from equicast.callbacks.step_timer import StepTimer
 
-__all__ = ["EMA", "StepTimer", "TimeDeltaCheckpoint"]
+__all__ = ["StepTimer", "TimeDeltaCheckpoint"]
