0cd1c255350fd8cae0c964594ae82f0f209f13a7
Mathias Schreiner <matschreiner@gmail.com>
2026-05-08 09:54:58 +0200

    sync experiments and jobs

--- bin/sync_leonardo.sh
+++ bin/sync_leonardo.sh
@@ -2,8 +2,5 @@
 # Sync mlflow db and job logs from Leonardo.
 REMOTE="leonardo:/leonardo/home/userexternal/jschrei1/equicast"
 
-# Checkpoint WAL into main db before syncing to avoid corruption.
-ssh leonardo "cd /leonardo/home/userexternal/jschrei1/equicast && sqlite3 mlflow/mlflow.db 'PRAGMA wal_checkpoint(FULL);'" 2>/dev/null
-
-rsync -avpr "$REMOTE/mlflow/" mlflow/
 rsync -avpr "$REMOTE/jobs/" jobs/
+rsync -avpr "$REMOTE/experiments/" experiments/
