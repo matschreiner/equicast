d22c19971828839653a6d059654bc2a000bf1090
Mathias Schreiner <matschreiner@gmail.com>
2026-05-20 15:53:28 +0200

    add possibility to add max time to interactive

--- bin/leonardo_interactive.sh
+++ bin/leonardo_interactive.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Interactive shell on Leonardo
-# Usage: ./scripts/leonardo_interactive.sh [gpus] [cpus]
+# Usage: ./scripts/leonardo_interactive.sh [gpus] [cpus] [time]
 #   gpus: number of GPUs (default: 0)
 #   cpus: number of CPUs (default: gpu*8 if gpus>0, else 1)
+#   time: wall time in HH:MM:SS (default: 01:00:00)
 
 GPUS=${1:-0}
 CPUS=${2:-$(( GPUS > 0 ? GPUS * 8 : 1 ))}
+TIME=${3:-01:00:00}
 
 GPU_FLAG=""
 if [ "$GPUS" -gt 0 ]; then
     GPU_FLAG="--gres=gpu:$GPUS"
 fi
 
-echo "Requesting: ${GPUS} GPU(s), ${CPUS} CPU(s), 1h"
-srun --nodes=1 --ntasks=1 $GPU_FLAG --cpus-per-task=$CPUS --time=01:00:00 --partition=boost_usr_prod --account=deste_340_26 --pty bash
+echo "Requesting: ${GPUS} GPU(s), ${CPUS} CPU(s), ${TIME}"
+srun --nodes=1 --ntasks=1 $GPU_FLAG --cpus-per-task=$CPUS --time=$TIME --partition=boost_usr_prod --account=deste_340_26 --pty bash
