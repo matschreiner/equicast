55c6429251795c7c56b8461d48da6a7b9c67ce00
Mathias Schreiner <matschreiner@gmail.com>
2026-06-01 10:04:17 +0200

    save forecasts

--- equicast/experiment/forecast.py
+++ equicast/experiment/forecast.py
@@ -1,5 +1,6 @@
 import os
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Any
 
 from equicast.data.data_handler import BaseDataHandler
@@ -16,7 +17,8 @@ class ForecastConfig(ExperimentConfig):
     output_dir: str = "forecasts"
 
     def run(self):
-        output_dir = f"{self.output_dir}/{self.model_id}"
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M")
+        output_dir = f"{self.output_dir}/{timestamp}_{self.model_id}"
         forecast = self.forecaster.forecast(timeseries=self.input_timeseries)
 
         forecast_path = os.path.join(output_dir, "forecast.zarr")
